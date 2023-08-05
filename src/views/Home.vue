dda0b073b32b0bac3e37b07ff94d820a70ae2b30
TA2LSM <semihsenol@gmail.com>
2023-08-05 11:49:51 +0300

    commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="home">
+    <!-- name.value and age.value will be automatically used in the template -->
     <p ref="refP">Name: {{ name }}, Age: {{ age }}</p>
 
-    <button @click="handleClick">
-      Click me!
-    </button>
+    <button @click="handleClick">Click me!</button>
+    <button @click="++age" style="margin-left: 4px;">Increase age</button>
+
+    <!-- name in the input field will be updated in the DOM -->
+    <input type="text" style="margin-left: 4px;" v-model="name">
+
   </div>
 </template>
 
@@ -25,31 +29,47 @@ export default {
     // let age_in_setup = 39
     // return { name: name_in_setup, age: age_in_setup}
     
-    // variables
-    let name = 'ta2lsm'
-    let age = 39
+    // variables (not reactive. they can be changed but will not be updated in the DOM)
+    // let name = 'ta2lsm'
+    // let age = 39
+
+    const name = ref('ta2lsm')
+    const age = ref(39)
+
+    // refs
+    const refP = ref(null)
+    //console.log(refP, refP.value)   // can't use refP before returns it below. refP will be null at this point...
 
     // functions
     const handleClick = () => {
-      // console.log('Button clicked')
-      console.log("refP: ", refP)
-      console.log("refP Value: ", refP.value)
-    }
+      // // console.log('Button clicked')
+      // console.log("refP: ", refP)
+      // console.log("refP Value: ", refP.value)
 
-    // refs
-    const refP = ref(null)    // 15. dk devam
+      // refP.value.classList.add('test')                // add class named 'test' to paragraph
+      // refP.value.textContent = 'NEW TEXT CONTENT'     // change paragraph's text content
+
+      // name = 'John'         // this will change value of name but will not be updates in the DOM
+      name.value = 'John'     // if "name" is defined as a ref, when we change its value it will be updated in the DOM also
+      age.value = 18
+    }
 
     return { name, age, handleClick, refP }
   },
-  // // 2nd order
-  // data() {
-  //   // values in data are reactive values. Changes of them will be reflected in template
-  //   // ...
-  // },
+
+  // 2nd order
+  data() {
+    return {
+      // values in data are reactive values. Changes of them will be reflected in template
+      score: 5
+    }
+  },
+
   // // 3rd order
   // created() {
 
   // },
+
   // // 4th order
   // mounted() {
 
